2c5cd69f4c38b810ac7641b4d7204e704fc77928
Deepaliiiii <147410192+Deepaliiiii@users.noreply.github.com>
2026-09-19 14:50:50 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://homely-hub-1-gd0r.onrender.com',
         changeOrigin: true,
         secure: false,
       }
