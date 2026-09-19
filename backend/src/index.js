c81ee3b7e2c75a0dd7a8673267e7928303a1fe8a
Deepali M <deepalim230@gmail.com>
2026-09-19 14:39:33 +0530

    Updated code

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,6 +31,11 @@ app.use(express.urlencoded({limit:"100mb", extended:true}))
 //cookieParser
 app.use(cookieParser())
 
+app.use(cors({
+  origin:process.env.ORIGIN_ACCESS_URL,
+  credentials:true
+}))
+
 const port = process.env.PORT || 8080;
 
 
